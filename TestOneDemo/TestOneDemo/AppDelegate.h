f14bab8dd954c345ecfa39a3d0f95b697cb7e728
phyllis <phyllis@163.com>
2015-12-30 17:27:20 +0800

    创建一个testdemo

---
+++ TestOneDemo/TestOneDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestOneDemo
+//
+//  Created by Yuu_zhang on 15/12/30.
+//  Copyright © 2015年 Phyllis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestOneDemo/TestOneDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestOneDemo
+//
+//  Created by Yuu_zhang on 15/12/30.
+//  Copyright © 2015年 Phyllis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
