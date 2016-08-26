946f3111998e997c48ecc3497b03aba1d39c3944
phyllis <phyllis_yuu@163.com>
2016-08-26 17:09:45 +0800

    no message

---
+++ TestOneDemo/TestOneDemo/GuideManager.swift
@@ -0,0 +1,13 @@
+//
+//  GuideManager.swift
+//  TestOneDemo
+//
+//  Created by xin on 16/8/12.
+//  Copyright © 2016年 Phyllis. All rights reserved.
+//
+
+import Cocoa
+
+class GuideManager: NSObject {
+
+}
